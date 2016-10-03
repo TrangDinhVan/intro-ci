101a55d47d33428d780de6dc41909ef2c2cdeb87
Trang Dinh Van <trangdv@outlook.com>
2016-10-03 12:40:41 +0700

    LESS: added

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,10 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	public function index(){
-		$data = array();
-		$data['page_title'] = 'Đăng nhập - Intro Code Igniter';
-		$this->load->view('header', $data);
-		$this->load->view('authen/login-form', $data);
-		$this->load->view('footer', $data);
+        $this->login_form();
 	}
+    public function login_form($mes = null){
+        $data = array();
+        $data['page_title'] = 'Đăng nhập - Intro Code Igniter';
+        $data['mes'] = '';
+        if( $mes ):
+            $data['mes'] = $mes;
+        endif;
+        $this->load->view('header', $data);
+        $this->load->view('authen/login-form', $data);
+        $this->load->view('footer', $data);
+    }
 }
--- application/views/authen/login-form.php
+++ application/views/authen/login-form.php
@@ -1,2 +1,19 @@
-Login form
-<div class="label label-danger">Gogo</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <form action="<?php echo base_url()."index.php/user/login/" ?>" method="POST" role="form" id="login-form">
+                <legend><h1 class="text-center text-success">Đăng nhập</h1></legend>
+                <?php echo $mes; ?>
+                <div class="form-group">
+                    <label for="name">Tên hoặc email: </label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Ví dụ: yourname@info.com" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Mật khẩu: </label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Mật khẩu của bạn" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
